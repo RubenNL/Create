6b03ea3bb04afa5933fb400aabb1bc327c9274d5
Ruben <ruben@rubend.nl>
2023-01-30 19:25:17 +0100

    Automatic assemble and disassemble trains by powering the station with redstone.

--- src/main/java/com/simibubi/create/content/logistics/trains/management/edgePoint/station/StationBlock.java
+++ src/main/java/com/simibubi/create/content/logistics/trains/management/edgePoint/station/StationBlock.java
@@ -6,15 +6,19 @@ import com.simibubi.create.AllSoundEvents;
 import com.simibubi.create.AllTileEntities;
 import com.simibubi.create.content.contraptions.wrench.IWrenchable;
 import com.simibubi.create.content.logistics.block.depot.SharedDepotBlockMethods;
+import com.simibubi.create.content.logistics.trains.entity.Train;
 import com.simibubi.create.foundation.advancement.AdvancementBehaviour;
 import com.simibubi.create.foundation.block.ITE;
 import com.simibubi.create.foundation.block.ProperWaterloggedBlock;
 import com.simibubi.create.foundation.gui.ScreenOpener;
 
+import com.simibubi.create.foundation.utility.Iterate;
+
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
@@ -32,6 +36,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.pathfinder.PathComputationType;
@@ -43,19 +48,65 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.DistExecutor;
 
+import java.util.Random;
+import java.util.UUID;
+
 public class StationBlock extends Block implements ITE<StationTileEntity>, IWrenchable, ProperWaterloggedBlock {
 
 	public static final BooleanProperty ASSEMBLING = BooleanProperty.create("assembling");
+	public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
 
 	public StationBlock(Properties p_54120_) {
 		super(p_54120_);
 		registerDefaultState(defaultBlockState().setValue(ASSEMBLING, false)
-			.setValue(WATERLOGGED, false));
+			.setValue(WATERLOGGED, false)
+			.setValue(POWERED, false));
+	}
+
+	private int getPower(Level worldIn, BlockPos pos) {
+		int power = 0;
+		for (Direction direction : Iterate.directions)
+			power = Math.max(worldIn.getSignal(pos.relative(direction), direction), power);
+		for (Direction direction : Iterate.directions)
+			power = Math.max(worldIn.getSignal(pos.relative(direction), Direction.UP), power);
+		return power;
+	}
+	@Override
+	public void neighborChanged(BlockState state, Level worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+		if (worldIn.isClientSide)
+			return;
+
+		if (!worldIn.getBlockTicks()
+				.willTickThisTick(pos, this))
+			worldIn.scheduleTick(pos, this, 0);
+	}
+	@Override
+	public void tick(BlockState state, ServerLevel worldIn, BlockPos pos, Random pRandom) {
+		if (worldIn.isClientSide)
+			return;
+		int power = getPower(worldIn, pos);
+
+		boolean previouslyPowered = state.getValue(POWERED);
+		if (previouslyPowered != power > 0) {
+			worldIn.setBlock(pos, state.cycle(POWERED), 2);
+			if(power > 0) withTileEntityDo(worldIn, pos, te -> {
+				if(te.trainCanDisassemble) {
+					GlobalStation station = te.getStation();
+					if(station==null) return;
+					Train train = station.getPresentTrain();
+					if(train==null) return;
+					train.disassemble(te.getAssemblyDirection(), te.edgePoint.getGlobalPosition().above());
+				} else {
+					te.assemble(UUID.randomUUID());
+				}
+			});
+		}
+
 	}
 
 	@Override
 	protected void createBlockStateDefinition(Builder<Block, BlockState> pBuilder) {
-		super.createBlockStateDefinition(pBuilder.add(ASSEMBLING, WATERLOGGED));
+		super.createBlockStateDefinition(pBuilder.add(ASSEMBLING, WATERLOGGED, POWERED));
 	}
 
 	@Override
@@ -69,7 +120,7 @@ public class StationBlock extends Block implements ITE<StationTileEntity>, IWren
 		updateWater(pLevel, pState, pCurrentPos);
 		return pState;
 	}
-	
+
 	@Override
 	public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, LivingEntity pPlacer, ItemStack pStack) {
 		super.setPlacedBy(pLevel, pPos, pState, pPlacer, pStack);
